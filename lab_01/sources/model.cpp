546505c52e5702442c6e46e7849b86a674d37a5b
d_ivahnencko <ivahnencko01@gmail.com>
2021-03-12 16:31:02 +0300

    DECOMPOSE: add load_model_stream

--- lab_01/sources/model.cpp
+++ lab_01/sources/model.cpp
@@ -3,37 +3,44 @@
 #include "lines.hpp"
 
 
-void init_model(model_t &model)
+model_t create_model()
 {
-    init_points(model.points);
-    init_lines(model.lines);
+    model_t new_model;
+
+    init_points(new_model.points);
+    init_lines(new_model.lines);
+
+    return new_model;
+}
+
+static err_t load_model_stream(model_t &model, FILE *datafile)
+{
+    err_t rc = SUCCESS;
+    if ((rc = load_points(model.points, datafile)) != SUCCESS)
+        return rc;
+
+    if ((rc = load_lines(model.lines, datafile)) != SUCCESS)
+        return rc;
+
+    return SUCCESS;
 }
 
 err_t load_model(model_t &model, filename_t fname)
 {
-    FILE *datafile = NULL;
-    if ((datafile = fopen(fname, "r")) == NULL)
-        return ERR_FOPEN;
+    FILE *datafile = fopen(fname, "r");
 
-    model_t temp_model;
-    init_model(temp_model);
+    if (!datafile) return ERR_FOPEN;
 
-    err_t rc = SUCCESS;
-    if ((rc = load_points(temp_model.points, datafile)) != SUCCESS)
+    model_t temp_model = create_model();
+    err_t rc = load_model_stream(temp_model, datafile);
+    fclose(datafile);
+
+    if (rc != SUCCESS)
     {
-        fclose(datafile);
-        return rc;
-    }
-
-    if ((rc = load_lines(temp_model.lines, datafile)) != SUCCESS)
-    {
-        fclose(datafile);
         destruct_model(temp_model);
         return rc;
     }
-
-    fclose(datafile);
-
+   
     destruct_model(model);
     model = temp_model;
 
@@ -45,7 +52,6 @@ err_t redraw_model(const model_t &model, const canvas_t &canvas)
     if (!(points_are_defined(model.points) && lines_are_defined(model.lines)))
         return ERR_MEMORY;
 
-    canvas->clear();
     drawer(model.lines, model.points, canvas);
 
     return SUCCESS;
@@ -61,22 +67,51 @@ err_t translate_model(model_t &model, const translate_t &tr_data)
     return SUCCESS;
 }
 
+static void translate_to_center(model_t &model, point_t &center)
+{
+    translate_t tr_data;
+    tr_data.x = -center.x;
+    tr_data.y = -center.y;
+    tr_data.z = -center.z;
+    
+    translate_model(model, tr_data);
+}
+
+
+static void translate_back(model_t &model, point_t &place)
+{
+    translate_t tr_data;
+    tr_data.x = place.x;
+    tr_data.y = place.y;
+    tr_data.z = place.z;
+    
+    translate_model(model, tr_data);
+}
+
 err_t scale_model(model_t &model, const scale_t &sc_data)
 {
+    point_t center = get_center(model.points);
+    translate_to_center(model, center);
+
     if (!(points_are_defined(model.points) && lines_are_defined(model.lines)))
         return ERR_MEMORY;
 
     scale_points(model.points, sc_data);
+    translate_back(model, center);
 
     return SUCCESS;
 }
 
 err_t rotate_model(model_t &model, const rotate_t &rot_data)
 {
+    point_t center = get_center(model.points);
+    translate_to_center(model, center);
+
     if (!(points_are_defined(model.points) && lines_are_defined(model.lines)))
         return ERR_MEMORY;
 
     rotate_points(model.points, rot_data);
+    translate_back(model, center);
 
     return SUCCESS;
 }
