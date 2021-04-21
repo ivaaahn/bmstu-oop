aa8ec419bc279cc092612cd8ce46a06e32affab8
d_ivahnencko <ivahnencko01@gmail.com>
2021-04-21 15:42:57 +0300

    ADD: Main with test runner

---
+++ lab_02/main.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "matrix.hpp"
+#include "matrix_tests.hpp"
+
+
+int main(int argc, char **argv) 
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
