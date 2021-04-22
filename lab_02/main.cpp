fdfd69dbb18dc27b339a98ec5955012822d5b2b4
d_ivahnencko <ivahnencko01@gmail.com>
2021-04-22 22:26:25 +0300

    REFACTORED

--- lab_02/main.cpp
+++ lab_02/main.cpp
@@ -3,8 +3,7 @@
 #include "matrix.hpp"
 #include "matrix_tests.hpp"
 
-
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
