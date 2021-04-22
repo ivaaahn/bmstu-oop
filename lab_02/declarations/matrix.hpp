1bbf1cfde45994258a5b3e8d2712593d64e18487
d_ivahnencko <ivahnencko01@gmail.com>
2021-04-22 04:01:08 +0300

    REWRITE: operator-, neg was added eye, zeros, minor. Btw exception calling was rewrited

--- lab_02/declarations/matrix.hpp
+++ lab_02/declarations/matrix.hpp
@@ -10,6 +10,8 @@
 #include "iterator.hpp"
 #include "const_iterator.hpp"
 
+using string = std::string;
+
 template<typename T>
 class Matrix : public MatrixBase
 {
@@ -22,12 +24,18 @@ private:
     std::shared_ptr<MatrixRow[]> allocMem(const size_t rows, const size_t cols);
     void _moveRow(const size_t from, const size_t to);
     void _moveCol(const size_t from, const size_t to);
-    void _checkIndexOutOfBound(const size_t pos, const size_t limit) const;
-    void _checkDimensionsEqu(const Matrix &matrix) const;
-    void _checkDimensionsForMult(const Matrix &matrix) const;
+    // void _checkIndexOutOfBound(const size_t pos, const size_t limit) const;
+    // void _checkDimensionsEqu(const Matrix &matrix) const;
+    // void _checkDimensionsForMult(const Matrix &matrix) const;
+    // void _checkSquare(const string errorMsg = "") const;
+    // void _checkSingular(const T &determinant, const string errorMsg = "") const;
+    bool _validInitList(std::initializer_list<std::initializer_list<T>> initList, const size_t rowSize) const;
+
+    // void _checkDimensionsForMult(const Matrix &matrix) const;
 
 
 public:
+    // TODO: a = {} and a{}. EXPLICIT???
     Matrix(const size_t rows=0, const size_t cols=0);
     Matrix(const size_t rows, const size_t cols, const T &filler);
     Matrix(std::initializer_list<std::initializer_list<T>> initList);
@@ -37,7 +45,6 @@ public:
     
 
     // TODO constructor for iterators
-    //? constructor for C-style
 
     virtual ~Matrix() override = default;
 
@@ -70,8 +77,8 @@ public:
     Matrix<T> divElem(const T &elem) const;
 
 
-    Matrix<T> operator-();
-    Matrix<T> neg();
+    Matrix<T> operator-() const;
+    Matrix<T> neg() const;
 
     Matrix<T> &operator+=(const Matrix &matrix);
     Matrix<T> &operator-=(const Matrix &matrix);
@@ -96,28 +103,34 @@ public:
 
     bool isSquare() const;
     T determinant() const;
-    void transpose();
+    T minor(const size_t row, const size_t col) const;
+    Matrix<T>& transpose();
     Matrix<T>& inverse();
 
-    // TODO: А надо ли???
-    ConstIterator<T> begin() const;
-    ConstIterator<T> end() const;
 
     Iterator<T> begin();
     Iterator<T> end();
+
     ConstIterator<T> cbegin() const;
     ConstIterator<T> cend() const;
 
-    void fill(Iterator<T> start, const Iterator<T> &end, const T &value);
+    void fill(Iterator<T> start, const Iterator<T> &end, const T &filler = {});
     void fill(Iterator<T> start, Iterator<T> source_start, const Iterator<T> &source_end);
     void fill(Iterator<T> start, ConstIterator<T> source_start, const ConstIterator<T> &source_end);
+
+    Matrix<T>& eye();
+    Matrix<T>& zeros();
 
     void resize(size_t newRows, size_t newCols, const T &filler = {});
     void resizeRows(const size_t new_size, const T &filler = {});
     void resizeCols(const size_t new_size, const T &filler = {});
 
     void insertRow(const size_t pos, const T &filler = {});
+    void insertRow(const size_t pos, std::initializer_list<T> lst);
+
     void insertCol(const size_t pos, const T &filler = {});
+    void insertCol(const size_t pos, std::initializer_list<T> lst);
+
 
     void deleteRow(const size_t pos);
     void deleteCol(const size_t pos);
@@ -126,11 +139,13 @@ public:
     bool operator!=(const Matrix& matrix) const;
 
 
+    // TODO: А чо по констам справа??
     MatrixRow operator[](const size_t row);
     const MatrixRow operator[](const size_t row) const;
 
     T& at(const size_t row, const size_t col);
     T& operator()(const size_t row, const size_t col);
+
     
     const T& at(const size_t row, const size_t col) const;
     const T& operator()(const size_t row, const size_t col) const;
@@ -160,14 +175,16 @@ public:
     };
 };
 
-#include "matrix_row.inl"
+#include "matrix_access_operators.inl"
+#include "matrix_assign_operators.inl"
 #include "matrix_checkers.inl"
 #include "matrix_construct.inl"
-#include "matrix_assign_operators.inl"
+#include "matrix_fill.inl"
 #include "matrix_iter.inl"
 #include "matrix_math.inl"
 #include "matrix_operations.inl"
 #include "matrix_restruct.inl"
+#include "matrix_row.inl"
 
 
 #endif  // __MATRIX_HPP__
